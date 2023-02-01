86981c5a762aee00ddfc21abf02ab7a9a82f3aeb
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-02-01 02:57:50 -0500

    changed settingd in js and html

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -2,10 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-let currentDay = dayjs()
-$('#currentDay').text(currentDay.format('MMM D, YYYY'));
-console.log(currentDay);
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -27,11 +23,35 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
+$(document).ready(function(){
 
-let nineTextAreaEl = $('#nineTextArea');
-let nineSaveBtnEl = $('$saveBtn');
+  $('.saveBtn').on('click', function(){
+    
+    let value = $(this).siblings('.description').val();
+    let time = $(this).parent().attr('id');
 
-nineSaveBtnEl.on('click', function(){
-  let 
+    localStorage.setItem(time,value);
+  });
 
-})
+
+
+
+
+
+
+  
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+
+  $('#currentDay').text(dayjs().format('dddd, MMM D, YYYY'));
+
+
+});
