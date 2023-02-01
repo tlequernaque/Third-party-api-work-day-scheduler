4b8a8cfef8f462dc47044a3537835d46cf6d4962
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-02-01 04:10:31 -0500

    added if and else statements in js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -3,28 +3,20 @@
 // in the html.
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+ 
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  
+
+  
 });
 
 $(document).ready(function(){
-
+  // added event listener for saving button function to localStorage
   $('.saveBtn').on('click', function(){
     
     let value = $(this).siblings('.description').val();
@@ -33,13 +25,32 @@ $(document).ready(function(){
     localStorage.setItem(time,value);
   });
 
+  function updateTimeBlock(){
+    let currentHour =dayjs.hour();
+    console.log(currentHour);
+
+    $(".time-block").each(function(){
+      let blockHour = parseInt($(this).attr("id").split("-")[1]);
+      console.log(blockHour);
+
+      if (blockHour < currentHour){
+        $(this).addClass("past");
+      } else if (blockHour === currentHour){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }else if (blockHour>currentHour){
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+
+  }
+
+  updateTimeBlock();
 
 
-
-
-
-
-  
+  // added code for input saved in localStorage to in text area 
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -50,8 +61,7 @@ $(document).ready(function(){
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-
+  // display the current date in the header of the page.
   $('#currentDay').text(dayjs().format('dddd, MMM D, YYYY'));
-
 
 });
