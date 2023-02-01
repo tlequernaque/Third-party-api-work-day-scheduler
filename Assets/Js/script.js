9e8692f5154741b392d30f316ee1b47fd8b3711a
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-01-31 19:23:44 -0500

    added content for more times in html

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+let currentDay = dayjs()
+$('#currentDay').text(currentDay.format('MMM D, YYYY'));
+console.log(currentDay);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +26,12 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+let nineTextAreaEl = $('#nineTextArea');
+let nineSaveBtnEl = $('$saveBtn');
+
+nineSaveBtnEl.on('click', function(){
+  let 
+
+})
